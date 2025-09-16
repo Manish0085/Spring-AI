6b13c35c1a1fd050da1357f9d2f39332efbeae42
Manish0085 <manishk57107@gmail.com>
2025-09-16 16:08:15 +0530

    Open ai Spring model

--- spring-ai-openai/src/main/java/com/spring/openai/spring_ai_openai/controller/ChatController.java
+++ spring-ai-openai/src/main/java/com/spring/openai/spring_ai_openai/controller/ChatController.java
@@ -1,4 +1,29 @@
 package com.spring.openai.spring_ai_openai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ChatController {
+
+    private ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping
+    public ResponseEntity<String> chat(
+            @RequestParam("q") String query
+    ){
+        String content = chatClient.prompt(query).call().content();
+        return ResponseEntity.ok(content);
+    }
+
+
 }
