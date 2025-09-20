58613712c038a277ec754880ae6083de7c671655
Manish0085 <manishk57107@gmail.com>
2025-09-20 18:35:52 +0530

    Creating Custom Advisor

---
+++ custom-advisor/src/main/java/com/spring/chatclient_api/service/ChatService.java
@@ -0,0 +1,12 @@
+package com.spring.chatclient_api.service;
+
+import com.spring.chatclient_api.entity.Tut;
+
+import java.util.List;
+
+public interface ChatService {
+
+
+
+    String chatTemplate(String query);
+}
